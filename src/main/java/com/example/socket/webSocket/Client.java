27e4431265219eb1d11bb0c3576f57650eefe85a
Samvel7777 <63920815+Samvel7777@users.noreply.github.com>
2021-07-09 11:55:33 +0400

    server - client

---
+++ src/main/java/com/example/socket/webSocket/Client.java
@@ -0,0 +1,40 @@
+package com.example.socket.webSocket;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+    private static Socket clientSocket;
+    private static BufferedReader reader;
+    private static BufferedReader in;
+    private static BufferedWriter out;
+
+    public static void main(String[] args) {
+        try {
+            try {
+                clientSocket = new Socket("localhost", 4004);
+                reader = new BufferedReader(new InputStreamReader(System.in));
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                System.out.println("Вы что-то хотели сказать? Введите это здесь:");
+
+                String word = reader.readLine();
+                out.write(word + "\n");
+                out.flush();
+                String serverWord = in.readLine();
+                System.out.println(serverWord);
+            } finally {
+                System.out.println("Клиент был закрыт...");
+                clientSocket.close();
+                in.close();
+                out.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
